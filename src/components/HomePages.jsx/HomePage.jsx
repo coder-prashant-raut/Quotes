a268fef01e30f3d56051adc29fc4fac7408224e7
Prashant Raut <prashantraut1308@gmail.com>
2025-03-17 09:29:44 +0530

    updated Quote Card UI

--- src/components/HomePages.jsx/HomePage.jsx
+++ src/components/HomePages.jsx/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 
 function HomePage() {
@@ -12,6 +12,7 @@ function HomePage() {
       Image()
   };
 
+  
   const Image = () => {
     fetch("https://api.allorigins.win/get?url=https://random-image-pepebigotes.vercel.app/api/random-image")
     .then((res) => res.json())
@@ -22,6 +23,11 @@ function HomePage() {
 
  console.log(quote);
  console.log(image);
+
+ useEffect(() => {
+   Quote(), Image()
+ }, [])
+ 
  
  
   
@@ -29,30 +35,27 @@ function HomePage() {
     <div>
       <Navbar />
 
-      <div className="">
-      <div className="w-200 min-h-screen bg-gray-200 flex items-center justify-center px-5 py-5">
+      <div className="w-200 min-h-screen bg-gray-800 flex items-center justify-center px-5 py-5">
     <div className="w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" >
         <div className="w-full pt-1 pb-5">
-            <div className="overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg">
-                <img src="https://randomuser.me/api/portraits/men/15.jpg" alt=""/>
+            <div className="overflow-hidden flex justify-center">
+            <button className="bg-amber-300 p-2 mx-auto shadow-lg cursor-pointer rounded-sm hover:bg-amber-500" onClick={Quote}>Gen Quote</button> 
+
             </div>
         </div>
         <div className="w-full mb-10">
             <div className="text-3xl text-indigo-500 text-left leading-tight h-3">“</div>
-            <p className="text-sm text-gray-600 text-center px-5">{quote.quote}</p>
+            <p className="text-lg text-gray-600 text-center px-5">{quote.quote}</p>
             <div className="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">”</div>
         </div>
         <div className="w-full">
             <p className="text-md text-indigo-500 font-bold text-center">{quote.author}</p>
-        <button className=" " onClick={Quote}>Gen Quote</button> 
-           
+       
         </div>
     </div>
 </div>
 
-
-      </div>
-    </div>
+</div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,10 @@ function Navbar() {
   return (
     <div>
         <nav className="border-gray-200 bg-gray-900 h-15 ">
-    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl ">
-        <a className="flex items-center space-x-3 rtl:space-x-reverse p-10">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 p-50" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
-       
-    </div>
-</nav>
-
+        <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl pt-2 ">
+            <span className="self-center text-2xl font-semibold  dark:text-white">Flowbite</span>
+        </div>
+        </nav>
     </div>
   )
 }
